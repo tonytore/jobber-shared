8bec4e81848f9625a4daa1364c716cd7136bf493
tonytor <teme4842@gmail.com>
2026-01-15 19:32:39 +0300

    refactor: Relocate build scripts, update Babel preset configuration, adjust tsconfig, and streamline .gitignore.

---
+++ scripts/babel-preset.js
@@ -0,0 +1,33 @@
+const BABEL_ENV = process.env.BABEL_ENV;
+const isCommonJS = BABEL_ENV === 'cjs';
+const isESM = BABEL_ENV === 'esm';
+
+module.exports = () => ({
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        bugfixes: true,
+        loose: true,
+        modules: isCommonJS ? 'commonjs' : false,
+        targets: {
+          esmodules: isESM || undefined,
+          chrome: 70,
+        },
+      },
+    ],
+    '@babel/preset-typescript',
+
+  ],
+  // plugins: [
+  //   [
+  //     'module-resolver',
+  //     {
+  //       root: ['./src'],
+  //       alias: {
+  //         '@': './src'
+  //       }
+  //     }
+  //   ]
+  // ]
+});
